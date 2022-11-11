b8b03d2d4a0459b12fa297e17f643a7200260639
jtompkins415 <john.tompkins415@gmail.com>
2022-11-11 09:02:34 -0800

    begin writing tests

---
+++ helpers/sql.test.js
@@ -0,0 +1,9 @@
+const { BadRequestError } = require('../expressError');
+const {sqlForPartialUpdate} = require('./sql');
+
+
+describe('sqlForPartialUpdate', () => {
+    test('works', async () => {
+        
+    })
+})
